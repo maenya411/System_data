f3802a531d982c1306c2f0e2351bb43eef4f17d0
maenya411 <maenyaclinton@gmail.com>
2026-02-20 14:59:10 +0300

    changed the capslock into smaller to avoid error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-css: ['@/Assets/css/main.css'],
+css: ['@/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   vite: {
