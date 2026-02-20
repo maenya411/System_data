ccd0cf2418ff9dd8f58d0b9aa10e0f08b2ea0646
maenya411 <maenyaclinton@gmail.com>
2026-02-20 17:30:07 +0300

    removed navbar

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, computed } from 'vue'
 import Sidebar from '@/components/sidebar.vue'
-import Navbar from '~/components/Navbar.vue'
+
 import AddEmailModal from '@/components/AddEmailModal.vue'
 import EmailAnalytics from '@/components/EmailAnalytics.vue'
 import EmailTable from '@/components/EmailTable.vue'
